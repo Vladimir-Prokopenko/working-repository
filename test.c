96c61c3edfaa15783df20b204e333b4019ca72d2
Владимир Прокопенко <profcompanysumy@gmail.com>
2025-04-07 21:18:00 +0300

    ok

--- test.c
+++ test.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
 
 int main()
 
 {
-    // Чи ідентичні ці записи i += 1 та i++
-    int i = 1;
-    // printf("%d\t%d\n", i += 1, i++);
-    // printf("%d\t%d\n", i += 1, i += 1);
-    // printf("%d\t%d\n", i++, i++);
-    printf("%d\t%d\n", i++, i += 1);
+ 
 
     return 0;
 }
