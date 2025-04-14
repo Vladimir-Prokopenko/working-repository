0de5fbe40c89073f1817ef2151158740ec830c7e
Владимир Прокопенко <profcompanysumy@gmail.com>
2025-04-14 07:46:37 +0300

    ok

--- test.c
+++ test.c
@@ -1,16 +1,13 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <math.h>
+// #include <stdio.h>
+// #include <stdbool.h>
+// #include <math.h>
+// int printf(const char *format, ...);
 
 int main()
 
 {
 
-    // Яке значення отримає змінна b після виконання цього коду ?
-    int a = 0;
-    int b = !a + a;
-    a = a + b;
-    printf("%d", a);
+    printf("Hello, World!\n");
 
     return 0;
 }
