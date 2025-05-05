89de69a258cf2d4fff6a7713b9c3473ea0f13b76
Владимир Прокопенко <profcompanysumy@gmail.com>
2025-05-05 08:00:29 +0300

    ok

--- test.c
+++ test.c
@@ -6,11 +6,11 @@ int main()
 {
     system("chcp 65001");
 
-    // Користувач вводить тризначне число. Обчисліть та виведіть суму його цифр
-
-    // int hundreds = (num / 10^2) % 10;
-    // int tens = (num / 10^1) % 10;
-    // int ones = (num / 10^0) % 10;
+    int i;
+    for (i = 0; i < 5; ++i)
+        if (i == 3)
+            break; 0;
+    printf("%d ", i);
 
     return 0;
 }
