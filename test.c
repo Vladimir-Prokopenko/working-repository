2516218cf3793d8f27faa929501449bf563e64ef
Владимир Прокопенко <profcompanysumy@gmail.com>
2025-05-09 08:26:04 +0300

    ok

--- test.c
+++ test.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdbool.h>
+int x;
 
 int main()
 
 {
     // system("chcp 65001");
-
-    int i, j = 10;
-    for (i = 0; i < 3; i++)
+    int a = 5;
+    if (++a > 5 && ++a > 6 && a = 0)
     {
-
-        printf("%d ", ++j);
+        printf("Умова істина\n");
     }
+    else
+    {
+        printf("Умова хибна\n");
+    }
+    printf("Кінцеве значення a: %d\n", a);
+
+    return 0;
 }
