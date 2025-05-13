3052fe14122f7674d88ad3773a74c054666f3aab
Владимир Прокопенко <profcompanysumy@gmail.com>
2025-05-13 08:28:19 +0300

    ok

--- test.c
+++ test.c
@@ -8,10 +8,11 @@ int main()
 
 {
     // system("chcp 65001");
+    // int hundreds = (num / 256^2) % 256;
+    // int tens = (num / 256^1) % 256;
+    // int ones = (num / 256^0) % 256;
 
-    int a = 5;
-    int result = SQUARE(a + 1);
-    printf("%d ", result);
+    // printf("Сума цифр: %d\n", hundreds + tens + ones);
 
     return 0;
 }
