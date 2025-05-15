8d9541e260ab35ad3bb08342cd2402241029a354
Владимир Прокопенко <profcompanysumy@gmail.com>
2025-05-15 11:37:24 +0300

    ok

--- test.c
+++ test.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdbool.h>
-
-#define SQUARE(x) (x * x)
+#define VALUE 2
+#if VALUE 2
+printf("High\n");
+#else
+printf("Low\n");
+#endif
 
 int main()
 
 {
-    // system("chcp 65001");
-    // int hundreds = (num / 256^2) % 256;
-    // int tens = (num / 256^1) % 256;
-    // int ones = (num / 256^0) % 256;
 
-    // printf("Сума цифр: %d\n", hundreds + tens + ones);
 
     return 0;
 }
